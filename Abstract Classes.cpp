e663b5a9ab1bfcd71e586b96b01e00ab7273d8f9
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-04-19 14:41:13 +0530

    Create Abstract Classes.cpp
    Objective
    Today, we will extend what we learned yesterday about Inheritance to Abstract Classes. Because this is a very specific object oriented concept, submissions are limited to the few languages that use this construct. Check out the Tutorial tab for learning materials and an instructional video.
    
    Task
    Given a Book class and a Solution class, write a MyBook class that does the following:
    
    Inherits from Book
    Has a parameterized constructor taking these  parameters:
    string 
    string 
    int 
    Implements the Book class' abstract display() method so it prints these  lines:
    , a space, and then the current instance's .
    , a space, and then the current instance's .
    , a space, and then the current instance's .
    Note: Because these classes are being written in the same file, you must not use an access modifier (e.g.: ) when declaring MyBook or your code will not execute.
    
    Input Format
    
    You are not responsible for reading any input from stdin. The Solution class creates a Book object and calls the MyBook class constructor (passing it the necessary arguments). It then calls the display method on the Book object.
    
    Output Format
    
    The  method should print and label the respective , , and  of the MyBook object's instance (with each value on its own line) like so:

---
+++ Abstract Classes.cpp
@@ -0,0 +1,61 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Book{
+    protected:
+        string title;
+        string author;
+    public:
+        Book(string t,string a){
+            title=t;
+            author=a;
+        }
+        virtual void display()=0;
+
+};
+
+class MyBook : Book{
+
+    private:
+        int price;
+    public :
+        MyBook(string title_,string author_,int price_) : Book(title_,author_) ,price(price_){}
+
+        virtual void display(){
+            cout << "Title: " <<Book::title << "\nAuthor: " << Book::author << "\nPrice: " << price << endl;
+        }
+    
+};
+// Write your MyBook class here
+
+    //   Class Constructor
+    //   
+    //   Parameters:
+    //   title - The book's title.
+    //   author - The book's author.
+    //   price - The book's price.
+    //
+    // Write your constructor here
+    
+    
+    //   Function Name: display
+    //   Print the title, author, and price in the specified format.
+    //
+    // Write your method here
+    
+// End class
+
+int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
