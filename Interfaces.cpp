9639e9027ad15df12d511a3ad65a5e834d4d30b0
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-04-25 12:50:16 +0530

    Create Interfaces.cpp
    Objective
    Today, we're learning about Interfaces. Check out the Tutorial tab for learning materials and an instructional video!
    
    Task
    The AdvancedArithmetic interface and the method declaration for the abstract divisorSum(n) method are provided for you in the editor below.
    
    Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum(n) method must return the sum of all divisors of n.

---
+++ Interfaces.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+class Calculator : public AdvancedArithmetic {
+public:
+    int divisorSum(int n) {
+
+    int sum = n;
+    for (int i = 1, half = n/2; i <= half; i++) {
+      if (n % i == 0) sum += i;
+    }
+    return sum;
+  
+        
+        return 0;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
