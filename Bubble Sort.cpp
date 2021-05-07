e6f7fa0535902da8d5f3eb9d0da5d4b9af8f9435
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-05-07 19:13:30 +0530

    Create Bubble Sort.cpp
    Given an Integer N and a list arr. Sort the array using bubble sort algorithm.
    Output Status:
    Correct Answer.Correct Answer
    Execution Time:0.06

---
+++ Bubble Sort.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+      int i,j;
+      for( i=0;i<n-1;i++){
+          for(j=0;j<n-i-1;j++){
+              if(arr[j]>arr[j+1]){
+                  int temp = arr[j];
+                  arr[j] = arr[j+1];
+                  arr[j+1] = temp;
+              }
+          }
+      }
+    }
+};
