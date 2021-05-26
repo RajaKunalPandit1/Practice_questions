924f798fe3f412d279069e6f0db3d6b74211abb2
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-05-26 22:44:07 +0530

    Create Remove Duplicates from Sorted Array.cpp
    Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
    
    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
    
    Clarification:
    
    Confused why the returned value is an integer but your answer is an array?
    
    Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
    Output Status:
    Runtime: 8 ms, faster than 82.88% of C++ online submissions for Remove Duplicates from Sorted Array.
    Memory Usage: 13.7 MB, less than 21.47% of C++ online submissions for Remove Duplicates from Sorted Array.

---
+++ Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n= nums.size();
+        int count = 0;
+        for(int i = 1; i < n; i++){
+             if(nums[i] == nums[i-1]) count++;
+             else nums[i-count] = nums[i];
+}
+           return (n-count);
+        
+    }
+};
+
+// Another Approach
+
+int removeDuplicates(vector<int>& nums) {
+nums.erase( unique( nums.begin(), nums.end() ), nums.end() );
+return nums.size();
+}
