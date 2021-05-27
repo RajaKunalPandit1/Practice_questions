85be5bfbff88c7ba4493a25e8b17a4072a373953
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-05-27 10:33:17 +0530

    Update Find the Highest number .cpp
    Given an array in such a way that the elements stored in array are in increasing order initially and then after reaching to a peak element , elements stored are in decreasing order. Find the highest element.
    Note: A[i] != A[i+1]
     
    Output Status:
    Correct Answer.Correct Answer
    Execution Time:0.01

--- Find the Highest number .cpp
+++ Find the Highest number .cpp
@@ -20,3 +20,47 @@ public:
       
     }
 };
+
+// Alternative Solution
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+       int n= a.size();
+       int low=0,high= n-1;
+       
+       while(low<=high){
+           
+           int mid= (low+high)/2;
+           
+           if(a[mid]>a[mid+1] && a[mid]>a[mid-1]){
+               return a[mid];
+           }if(a[mid]<a[mid+1]){
+               low = mid+1;
+           }else{
+               high = mid-1;
+           }
+           
+       }
+    }
+};
+
+// Alternative Solution
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& a) 
+    {
+        int l=0;
+        int r=a.size()-1;
+        while(l<r){
+            int mid=(l+r)/2;
+            if(a[mid]>a[mid+1])r=mid;
+            else l=mid+1;
+        }
+        return a[l];
+    }
+    
+    
