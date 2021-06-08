07b812c21e7a0fc70378d7ea22b152b6dd16096b
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-06-08 12:04:50 +0530

    Create Find triplets with zero sum.cpp
    Given an array of integers. Check whether it contains a triplet that sums up to zero. 
    
    Example 1:
    
    Input: n = 5, arr[] = {0, -1, 2, -3, 1}
    Output: 1
    Explanation: 0, -1 and 1 forms a triplet
    with sum equal to 0.
    Example 2:
    
    Input: n = 3, arr[] = {1, 2, 3}
    Output: 0
    Explanation: No triplet with zero sum exists. 
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the boolean function findTriplets() which takes the array arr[] and the size of the array (n) as inputs and print 1 if the function returns true else print false if the function return false. 
    
    Expected Time Complexity: O(n2)
    Expected Auxiliary Space: O(1)
    
    Constrains:
    1 <= n <= 104
    -106 <= Ai <= 106
    Output Status:
    Correct Answer.Correct Answer
    Execution Time:0.22

---
+++ Find triplets with zero sum.cpp
@@ -0,0 +1,70 @@
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n)
+    { 
+       sort(arr,arr+n);
+       for(int i=0;i<n;i++){
+           int j=n-1,k=i+1;
+           while(k<j){
+               if(arr[i]+arr[j]+arr[k] == 0){
+                      return true;    
+               }else if(arr[i]+arr[j]+arr[k]>0){
+                   j--;
+               }else{
+                   k++;
+               }
+               
+           }
+       }
+       return false;
+    }
+};
+
+// Alternative Solution
+
+class Solution{
+  public:
+    //Function to find triplets with zero sum.
+    bool findTriplets(int arr[], int n){
+        
+        //Sorting the elements.
+        sort(arr, arr+n);
+        
+        //Traversing the array elements.
+        for (int i=0; i<n-2; i++)
+        {
+            //Taking two pointers. One at element after ith index and another
+            //at the last index.
+            int l = i + 1;
+            int r = n - 1;
+            int x = arr[i];
+            
+            //Using two pointers over the array which helps in checking if
+            //the triplet sum is zero.
+            while (l < r)
+            {
+                //If sum of elements at indexes i, l and r is 0, we return true.
+                if (x + arr[l] + arr[r] == 0)
+                {
+    				return true;
+                }
+                //Else if the sum is less than 0, it means we need to increase the
+                //sum so we increase the left pointer l.
+                else if (x + arr[l] + arr[r] < 0)
+                {
+                    l++;
+                }
+                //Else the sum is more than 0 and we need to decrease the
+                //sum so we decrease the right pointer r.
+                else
+                {
+                    r--;
+                }
+            }
+        }
+            //returning false if no triplet with zero sum is present.
+            return false;
+    }
+
+};
