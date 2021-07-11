b5829bf51f077b17bd4d972f675dc6b941f8d508
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-07-11 17:07:08 +0530

    Create Identical Linked Lists.cpp
    Given two Singly Linked List of N and M nodes respectively. The task is to check whether two linked lists are identical or not. 
    Two Linked Lists are identical when they have same data and with same arrangement too.
    
    Example 1:
    
    Input:
    LinkedList1: 1->2->3->4->5->6
    LinkedList2: 99->59->42->20
    Output: Not identical 
    Example 2:
    
    Input:
    LinkedList1: 1->2->3->4->5
    LinkedList2: 1->2->3->4->5
    Output: Identical
     
    Your Task:
    The task is to complete the function areIdentical() which takes the head of both linked lists as arguments and returns True or False.
    
    Constraints:
    1 <= N <= 103
    
    Expected Time Complexity : O(N)
    Expected Auxilliary Space : O(1)
    Output Status:
    Problem Solved Successfully 
    Total Points Scored:
    1/1
    Total Time Taken:
    0.2/1.3
    Your Accuracy:
    50%
    Attempts No.:
    2

---
+++ Identical Linked Lists.cpp
@@ -0,0 +1,51 @@
+/*
+Structure of the node of the linked list is as
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to check whether two linked lists are identical or not. 
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    if(head1 == NULL && head2== NULL){
+        return true;
+    }
+    
+     if(head1 == NULL && head2== NULL){
+        return false;
+    }
+    
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data != head2->data){
+            return false;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    return true;
+    
+}
+
+// Another Solution 
+
+bool areIdentical(struct Node *head1, struct Node *head2)
+{
+    //traversing through both the linked lists.
+	while (head1 != NULL && head2 != NULL)      
+	{
+	    //if data for current node in both lists are not equal, we return false.
+		if (head1->data != head2->data)         
+			return false;
+		head1 = head1->next;
+		head2 = head2->next;
+	}
+	//if both lists reach end after same number of traversals
+	//then true is returned else false.
+	return (head1 == NULL && head2 == NULL);  
+}
