c0f8b6e7d414e3661f066645f636f585c4620736
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-08-09 13:53:19 +0530

    Create Remainder Evaluation.cpp
    Given two positive integers num1 and num2, the task is to find the remainder when num1 is divided by num2.
    
    Example 1:
    
    Input:
    num1 = 5
    num2 = 3
    
    Output:
    2
    
    Explanation:
    The remainder after dividing 5 by 3 is: 2.
    Example 2:
    
    Input:
    num1 = 5
    num2 = 10
    
    Output:
    5
    
    Explanation:
    5 cannot be divided by 10 so it is 
    the Remainder.
    Your Task:  
    You don't need to read input or print anything. Your task is to complete the function findRemainder() which takes two integer num1 and num2 as an input parameter and return the remainder when num1 is divided by num2.
    
    Expected Time Complexity: O(1)
    Expected Auxiliary Space: O(1)
    
    Constraints:
    0 <= num1 <= 106
    1 <= num2 <= 106
    
    Output Status:
    Problem Solved Successfully 
    You get marks only for the first correct submission if you solve the problem without viewing the full solution.
    Total Time Taken:
    0.0/1.8
    Correct Submission Count:
    2
    Attempts No.:
    2

---
+++ Remainder Evaluation.cpp
@@ -0,0 +1,30 @@
+Output Status:
+Problem Solved Successfully 
+You get marks only for the first correct submission if you solve the problem without viewing the full solution.
+Total Time Taken:
+0.0/1.8
+Correct Submission Count:
+2
+Attempts No.:
+2
+
+class Solution
+{
+public:
+    int findRemainder(int num1,int num2)
+    {
+        return (num1%num2);
+    }
+};
+
+// Another Solution 
+
+class Solution
+{
+public:
+    int findRemainder(int num1,int num2)
+    {
+        int rem=num1%num2;
+        return rem;
+    }
+};
