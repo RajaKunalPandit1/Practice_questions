c4e6c80ddb90a615433735d24b4aa90c1aa39b40
Raja Kunal Pandit <76692648+RajaKunalPandit1@users.noreply.github.com>
2021-08-24 20:52:13 +0530

    Create Modulus of two double numbers .cpp
    Given two floating point numbers a and b, find a%b.
    
     
    
    Example 1:
    
    Input:
    a = 36.5, b = 5.0
    Output:
    1.5
    Explanation:
    36.5 % 5.0 = 1.5
    Example 2:
    
    Input:
    a = 9.7, b = 2.3
    Output:
    0.5
    Explanation:
    9.7 % 2.3 = 0.5
     
    
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function floatMod() which takes 2 doubles a, and b as input and returns a%b.
    
     
    
    Expected Time Complexity: O(1)
    Expected Auxiliary Space: O(1)
    
     
    
    Constraints:
    1 <= a,b<= 103
    
    Output Status:
    
    Problem Solved Successfully 
    Total Points Scored:
    1/1
    Total Time Taken:
    0.0/1.1
    Your Accuracy:
    100%
    Attempts No.:
    1

---
+++ Modulus of two double numbers .cpp
@@ -0,0 +1,19 @@
+Output Status:
+
+Problem Solved Successfully 
+Total Points Scored:
+1/1
+Total Time Taken:
+0.0/1.1
+Your Accuracy:
+100%
+Attempts No.:
+1
+  
+  class Solution {
+  public:
+    double floatMod(double a, double b) {
+        
+        return fmod(a,b);
+    }
+};
